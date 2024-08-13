0dc371b658d91ced174de671086f76795ee7d708
jkdellow <jennifer.dellow@gmail.com>
2024-08-13 12:16:43 -0400

    importing css

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: [
+      '.*.html',
+    ],
+    theme: {
+      extend: {
+        colors: {
+          'text': '#000000',
+          'accent': '#64a4ac',
+          'background': '#ffffff',
+        },
+        listStyleType: {
+          'oDisc': 'circle',
+        },
+        fontFamily: {
+          'sans': 'Noto Sans, Open Sans, Helvetica Neue, sans-serif',
+          'headers': 'Roboto Slab, Open Sans, Helvetica Neue, sans-serif',
+        }
+      },
+    },
+    plugins: [
+    ],
+  }
+  
