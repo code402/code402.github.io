d6d711e453b4230a21c8d01fc03c24437aeda45d
jkdellow <jennifer.dellow@gmail.com>
2024-09-05 11:11:12 -0400

    brand colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
           'text': '#000000',
           'accent': '#64a4ac',
           'background': '#ffffff',
+          'teal1': '#0d7490',
+          'magenta1': '#cd087d',
+          'blue1': '#a1d6e2',
+          'yellow1': '#ffcf48',
         },
         listStyleType: {
           'oDisc': 'circle',
